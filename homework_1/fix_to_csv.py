02f90a3b3b00b0d81a1277d47e9d11804b812aa7
amclaugh26 <andrewmclaughlin@uchicago.edu>
2025-10-13 16:08:48 -0500

    updated fix_to_csv.py to filter out irrelvant items in parse and return a csv with the correct headings and order

--- homework_1/fix_to_csv.py
+++ homework_1/fix_to_csv.py
@@ -13,7 +13,9 @@ import argparse
 
 def fix_to_csv(input_file, output_file):
     '''
-    Convert a .fix file to a .csv file.
+    takes a .fix file and returns a .csv file.
+
+    [add all the caveats from the hw description here]
 
     Parameters:
     input_file (str): Path to the input .fix file.
@@ -22,11 +24,19 @@ def fix_to_csv(input_file, output_file):
     try:
 
         with open(input_file, 'r') as fix_file, open(output_file, 'w') as csv_file:
+            headers = "OrderID,OrderTransactTime,ExecutionTransactTime,Symbol,Side,OrderQty,LimitPrice,AvgPx,LastMkt"
+            csv_file.write(headers + '\n')  # Write headers to the CSV file
             for line in fix_file:
-                # Replace the FIX delimiter (ASCII 1) with a comma
-                csv_line = line.replace('\x01', ',')
-                # Write the modified line to the CSV file
-                csv_file.write(csv_line)    
+                cleaned_list = []
+                list = line.split('\x01')
+
+                if "35=D" or ("35=8" and "150=2" and "39=2" and "40=2") in list:
+                    for item in list:
+                        if item.startswith("11=") or item.startswith("60=") or item.startswith("55=") or item.startswith("54=") or item.startswith("38=") or item.startswith("44=") or item.startswith("6=") or item.startswith("30="):
+                           cleaned_list.append(item[3:]) #append the value after the '='
+                    ordered_list = [cleaned_list[1], cleaned_list[7], cleaned_list[6], cleaned_list[5], cleaned_list[3], cleaned_list[4], cleaned_list[0], cleaned_list[2]]
+                    csv_line = ','.join(ordered_list) + '\n'
+                    csv_file.write(csv_line)    
 
 
     except Exception as files_are_unable_to_be_converted_error:  
