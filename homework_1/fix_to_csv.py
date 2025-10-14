d8cc057a8d0b443b696ec90dd70af671c31ccf17
amclaugh26 <andrewmclaughlin@uchicago.edu>
2025-10-14 16:20:28 -0500

    added comments

--- homework_1/fix_to_csv.py
+++ homework_1/fix_to_csv.py
@@ -25,27 +25,37 @@ def fix_to_csv(input_file, output_file):
     
     try:
         print("Opening File")
+        #open file
         with open(input_file, 'r') as fix_file, open(output_file, 'w') as csv_file:      
             print('File Opened')
             counter = 0
             orders = defaultdict(list)
             print("Parsing File...")
+
+
+            #Line by line analysis
             for line in fix_file:
+
+                #counter to check on progress (can comment out if unwanted)
                 counter += 1
                 if counter % 20000==0:
                     print(f"...parsed {counter} rows")
+
+                #manipulate message to be easily readible by system
                 fix_message = line.split('\x01')
                 fix_dict = {code.split("=")[0]: code.split("=")[1] for code in fix_message if "=" in code}
+
+                #create entries for new orders
                 if fix_dict.get("35") == "D":
                     order_id = fix_dict.get("11")
                     order_time = fix_dict.get("60")
-                    #create an entry in the dictionary with order and id and order time
                     orders[order_id].append({'OrderID': order_id, 'OrderTransactTime': order_time})
 
+
+                #find the fill for a new order
                 if fix_dict.get("35") == "8" and fix_dict.get("150") == "2" and fix_dict.get("39") == "2" and fix_dict.get("40") == "2":
                     order_id = fix_dict.get("11")
-                    #see if the order id is in the dictionary, if so add the rest of the fields 
-                    if order_id in orders:
+                    if order_id in orders:              
                         exec_time = fix_dict.get("60")
                         symbol = fix_dict.get("55")
                         side = fix_dict.get("54")
@@ -56,6 +66,7 @@ def fix_to_csv(input_file, output_file):
                         orders[order_id].append({'ExecutionTransactTime': exec_time, 'Symbol': symbol, 'Side': side, 'OrderQty': order_qty, 'LimitPrice': limit_price, 'AvgPx': avg_px, 'LastMkt': last_mkt})
             
             print("Completed parsing all rows\nConverting dictionaries to csv")
+
             # Flatten list of dicts per order_id (used google to help me with this)
             flattened_orders = {}
             for order_id, details_list in orders.items():
@@ -63,9 +74,10 @@ def fix_to_csv(input_file, output_file):
                 for d in details_list:
                     combined.update(d)  # merge all dictionaries
                 flattened_orders[order_id] = combined
+
+            #convert to dataframe and create csv
             df = pd.DataFrame.from_dict(flattened_orders, orient='index')
-            # Drop rows with missing values
-            df = df.dropna()
+            df = df.dropna()            # Drop orders that were not filled
             df = df.reset_index(drop=True)
             df.to_csv(output_file, index=False)
             print(f"{output_file} created")
