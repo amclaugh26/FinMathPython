bcb8788e3d0b5aa91e223c95fc64407cd55fabd3
amclaugh26 <andrewmclaughlin@uchicago.edu>
2025-10-14 16:23:27 -0500

    added more comments

--- homework_1/calc_execution_metrics.py
+++ homework_1/calc_execution_metrics.py
@@ -11,23 +11,35 @@ import argparse
 
 def calculate_metrics(input_csv_file, output_metrics_file):
     '''
-    Calculate execution metrics from FIX data CSV.'''
+    Calculate execution metrics from FIX data CSV.
+    
+    Parameters:
+    input_file (str): Path to the input .csv file.
+    output_file (str): Path to the output .csv file.
+    '''
 
     try:
+        #read the csv 
         df = pd.read_csv(input_csv_file, index_col="LastMkt")
+
+        #calculate speed
         df['ExecutionTransactTime'] = pd.to_datetime(df['ExecutionTransactTime'])
         df['OrderTransactTime'] = pd.to_datetime(df['OrderTransactTime'])
-        #calculate price improvement and average execution time for each LastMkt (broker)
-        metrics = pd.DataFrame(index=df.index.unique())
         df["exec_speed_secs"] = (df['ExecutionTransactTime'] - df['OrderTransactTime']).dt.total_seconds()
 
+        #calculate price improvements
         df["price_improvement"] = np.where(df['Side']==1, df['LimitPrice'] - df['AvgPx'], df['AvgPx'] - df['LimitPrice'])
+
+        #generate metrics dataframe
+        metrics = pd.DataFrame(index=df.index.unique())
         metrics['AvgExecSpeedSecs'] =df.groupby('LastMkt')['exec_speed_secs'].mean()
         metrics['AvgPriceImprovement'] = df.groupby('LastMkt')['price_improvement'].mean()
+
+        #publish to csv
         metrics.to_csv(output_metrics_file)
     
     except Exception as metric_error:  
-        print(f"Unable to calculate metrics with files provided")
+        print(f"Unable to calculate metrics with files provided, ensure input csv has correct headings")
 
 
 
